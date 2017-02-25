a904b59c35c4895731541e796b00e2107a18752c
naman14 <namandwivedi14@gmail.com>
2017-02-25 09:26:16 +0530

    setup firebase

--- electron/renderer.js
+++ electron/renderer.js
@@ -1,3 +1,30 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+let $ = require('jQuery');
+
+let firebase = require("firebase");
+
+let config = {
+    apiKey: "AIzaSyCWnIJ84XMljtxHEmZ4N4mkj-u36vxH7sQ",
+    authDomain: "csgolive-b4b34.firebaseapp.com",
+    databaseURL: "https://csgolive-b4b34.firebaseio.com/",
+};
+firebase.initializeApp(config);
+
+
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "medium");
+});
+
+$('#btn-signup').click(function () {
+    console.log("hehehe")
+    firebase.auth().createUserWithEmailAndPassword($('#signup-email').val(), $('#signup-password').val()).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("hehehe")
+        console.log(errorMessage)
+
+    });
+})
